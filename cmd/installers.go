6d940046c671d737daeec7689a99cd72eb2c4d97
William W. Marx <m94@marx.sh>
2022-12-09 19:39:26 -0600

    Add package manager control

--- cmd/installers.go
+++ cmd/installers.go
@@ -7,6 +7,48 @@ import (
 	"time"
 )
 
+////////////////////////////
+//         UTILS          //
+////////////////////////////
+
+// Download a file from this repo and return as byte array
+func download(path) []byte {
+	// Get request
+	url = "https://raw.githubusercontent.com/williamwmarx/shell/main/" + path
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read text from body and return
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return body
+}
+
+// Check if a command exists on the system
+func commandExists(commandName string) bool {
+	_, err := exec.LookPath(commandName)
+	return err == nil
+}
+
+// Run a system command
+func runCommand(command string) {
+	cmd := exec.Command(command)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+////////////////////////////
+//      GET PACKAGES      //
+////////////////////////////
+
+// Struct to hold all package info
 type (
 	pkgs struct {
 		Core      map[string]pkg
@@ -25,36 +67,88 @@ type (
 	}
 )
 
-func decode() {
+// Get all packages from TOML file
+func getPackages() pkgs {
+	// Download TOML file from this repo
+	tomlText := download("packages/packages.toml")
+
+	// Unmarshal TOML file into struct
 	var packages pkgs
-	_, err := toml.DecodeFile("packages/packages.toml", &packages)
+	_, err := toml.Decode(tomlText, &packages)
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, p := range packages.Core {
-		fmt.Println(p.Name)
+	return packages
+}
+
+var packages pkgs = getPackages()
+
+////////////////////////////
+//    INSTALL PACKAGES    //
+////////////////////////////
+
+// Keep track of package manager commands
+type packageManager struct {
+	name       string
+	installCmd string
+	updateCmd  string
+}
+
+// Install a package
+func (pm *packageManager) install(packageName string) {
+	runCommand(fmt.Sprintf("%s %s", pm.installCmd, packageName))
+}
+
+// Update pacakge manager
+func (pm *packageManager) update() {
+	runCommand(pm.updateCmd)
+}
+
+// Get system pacakge manager install command
+func getPackageManager() packageManager {
+	if commandExists("apt") {
+		return packageManager{
+			name: "apt",
+			installCmd: "apt install -y"
+			updateCmd: "apt update"
+		}
+	} else if commandExists("brew") {
+		return packageManager{
+			name: "brew",
+			installCmd: "brew install"
+			updateCmd: "brew upgrade"
+		}
+	} else if commandExists("dnf") {
+		return packageManager{
+			name: "dnf",
+			installCmd: "dnf install -y"
+			updateCmd: "dnf update"
+		}
+	} else if commandExists("pacman") {
+		return packageManager{
+			name: "pacman",
+			installCmd: "pacman -S --no-confirm"
+			updateCmd: "pacman -Syu"
+		}
 	}
 }
 
-func installZsh(temporary bool) {
-	time.Sleep(time.Millisecond * 500)
-	fmt.Println("------------- INSTALLING ZSH -----------------")
-}
+var pm packageManager = getPackageManager()
 
-func tmux(temporary bool) {
-	time.Sleep(time.Millisecond * 500)
-	if temporary {
-		fmt.Println("TEMPORARY TMUX")
-	} else {
-		fmt.Println("PERMANENT TMUX")
-	}
-}
-
-func vim(temporary bool) {
-	time.Sleep(time.Millisecond * 500)
-	if temporary {
-		fmt.Println("TEMPORARY VIM")
-	} else {
-		fmt.Println("PERMANENT VIM")
+// Handle the installing
+func install(packageGroups ...string) {
+	if len(packageGroups) > 0 {
+		// Update package manager
+		pm.update()
+		
+		// Gather packages to install
+		var packagesToInstall []string
+		for _, packageGroup := range packageGroups {
+			if packageGroup == "core" {
+				for _, p := range packages.Core {
+					packagesToInstall = append(packagesToInstall, p.Name)
+				}
+			}
+		}
 	}
 }
