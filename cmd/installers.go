4e9411739b025c8d394d48cf885476d4e203a356
William W. Marx <m94@marx.sh>
2022-12-09 18:13:30 -0600

    Add package TOML file decodign

--- cmd/installers.go
+++ cmd/installers.go
@@ -2,8 +2,39 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
+	"log"
 	"time"
 )
+
+type (
+	pkgs struct {
+		Core      map[string]pkg
+		Design    map[string]pkg
+		GuiCore   map[string]pkg `toml:"gui_core"`
+		GuiDesign map[string]pkg `toml:"gui_design"`
+	}
+
+	pkg struct {
+		Name         string
+		AptName      string `toml:"apt_name"`
+		BrewName     string `toml:"brew_name"`
+		BrewCaskName string `toml:"brew_cask_name"`
+		DnfName      string `toml:"dnf_name"`
+		PacmanName   string `toml:"pacman_name"`
+	}
+)
+
+func decode() {
+	var packages pkgs
+	_, err := toml.DecodeFile("packages/packages.toml", &packages)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, p := range packages.Core {
+		fmt.Println(p.Name)
+	}
+}
 
 func installZsh(temporary bool) {
 	time.Sleep(time.Millisecond * 500)
